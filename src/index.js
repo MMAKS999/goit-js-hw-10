13dce32a0f9d282a2e1dda7112266a1f9cc7dbf8
MAX999 <max999b@gmail.com>
2023-04-25 01:06:49 +0300

    hw10 bug
    сирий код з 90% функціоналу

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { debounce } from 'debounce';
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.getElementById('search-box');
 const list = document.querySelector('.country-list');
+const listInfo = document.querySelector('.country-info');
 // Функція генерування запиту за назвою країни
 const fetchCountries = name => {
   fetch(
@@ -15,8 +16,8 @@ const fetchCountries = name => {
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      listCountries(data[0]);
-      insertContent(data[0]);
+      // listCountries(data[1]);
+      selectionCountryFunction(data);
     });
 };
 
@@ -26,6 +27,10 @@ const numberCountriesMore = () => {
     'Too many matches found. Please enter a more specific name.'
   );
 };
+const CountriesNot = () => {
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+};
+
 // Функція генерування розмітки для країн 2-10
 const listCountries = countries => {
   console.log(countries.flags.svg);
@@ -41,23 +46,37 @@ const createListItem =
 <img src="${item.flags.svg}" alt="${item.flags.alt}" style="width: 30px; height: 20px; object-fit: cover; margin-right: 10px;">
 <span> ${item.name.official} </span>
 </li>`;
-// Функція розмноження рядка розмітки для всих елементів масиву
+// Функція розмноження рядка розмітки для всих елементів масиву країн
 const generateContent = array =>
   array.reduce((acc, item) => acc + createListItem(item), '');
 
 // функція генерування розмітки на 1 країну
-const ar1 = ['ofjf ', 'lklklk ', 'lklklk '];
-const createItem = item =>
-  `<img src="${item.flags.svg}" alt="${item.flags.alt}"style="width: 30px; object-fit: cover; margin-right: 10px;">
-  <h2> ${item.name.official} </h2>
+
+// функція створення масиву мов з обєкта
+const createItem = item => {
+  const arrayLang = Object.values(item.languages);
+  return arrayLang.map(word => ' ' + word);
+};
+const createItemHTML = itemOb => {
+  const item = itemOb[0];
+  const arrayLangSpaces = createItem(item);
+  return `
   <p> <span class="name-country-data">Capital:</span> ${item.capital} </p>
   <p> <span style="font-weight: bold;">Population: </span> ${item.population}</p>
-  <p> <span >Languages:</span> ${ar1}</p>`;
-// 
+  <p> <span style="font-weight: bold;">Languages:</span> ${arrayLangSpaces}</p>`;
+};
+//
 // функція додавання розмітки
 const insertContent = array => {
   // const result = generateContent(array);
-  const result = createItem(array);
+  const result = createItemHTML(array);
+  listInfo.innerHTML = '';
+  listInfo.insertAdjacentHTML('beforeend', result);
+};
+
+const insertContent1 = array => {
+  const result = generateContent(array);
+  clearList();
   list.insertAdjacentHTML('beforeend', result);
 };
 
@@ -70,17 +89,33 @@ const handleInputChange = event => {
     fetchCountries(name1);
   } else {
     numberCountriesMore();
+    clearList();
+  }
+};
+// Функція очищення вмісту елементів
+const clearList = () => {
+  list.innerHTML = '';
+  listInfo.innerHTML = '';
+};
+
+//  Функція перевірки кількості країн за запитом
+const selectionCountryFunction = array => {
+  if (array.length > 10) {
+    numberCountriesMore();
+    clearList();
+  } else if (array.length <= 10) {
+    insertContent1(array);
+    if (array.length === 1) {
+      insertContent(array);
+    }
+  } else {
+    CountriesNot();
+    clearList();
   }
 };
 // функція затримки обробки введеного тексту
-const debouncedInputChange = debounce(handleInputChange, 3000);
+const debouncedInputChange = debounce(handleInputChange, 300);
 // відслідковування ведденя назви країни
 inputEl.addEventListener('input', debouncedInputChange);
 
-Notiflix.Notify.success('Sol lucet omnibus');
-
-Notiflix.Notify.failure('Qui timide rogat docet negare');
-
-Notiflix.Notify.warning('Memento te hominem esse');
-
-Notiflix.Notify.info('Cogito ergo sum');
+// дописати класи на зміну розміру елементів при виборі 1єї країни, винести функцію в окремий док відповідності до тз 
