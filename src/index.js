b486a9d99c7fdad77f19553f433133e814614afd
MAX999 <max999b@gmail.com>
2023-04-26 00:38:23 +0300

    10hw
    На перевірку

--- src/index.js
+++ src/index.js
@@ -7,19 +7,43 @@ const DEBOUNCE_DELAY = 300;
 const inputEl = document.getElementById('search-box');
 const list = document.querySelector('.country-list');
 const listInfo = document.querySelector('.country-info');
+// Додавання стилів
+const styles = document.createElement('style');
+const styleText = `  
+  .country-list-element{
+      font-size: 26px;
+      font-weight: bold;
+  }
+   .country-list-element-img{
+      width: 30px;
+   }
+  
+.name-country-data{
+  font-weight: bold;
+}`;
+styles.textContent = styleText;
+document.head.appendChild(styles);
+
 // Функція генерування запиту за назвою країни
-const fetchCountries = name => {
-  fetch(
-    `https://restcountries.com/v3.1/name/${name}`
-    //  `https://restcountries.com/v3.1/name/${name}?fields=capital,population,flag,languages,name.official`
-  )
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      // listCountries(data[1]);
-      selectionCountryFunction(data);
-    });
-};
+import { fetchCountries } from './fetchCountries';
+// const fetchCountries = name => {
+//   fetch(
+//     `https://restcountries.com/v3.1/name/${name}`
+//     //  `https://restcountries.com/v3.1/name/${name}?fields=capital,population,flag,languages,name.official`
+//   )
+//     .then(response => {
+//       if (!response.ok) {
+//        throw new Error(response.status);
+//       } 
+//       return response.json();
+//     })
+//     .then(data => {
+//       selectionCountryFunction(data);
+//     })
+//     .catch((error) => {
+//       CountriesNot();
+//     });
+// };
 
 // Функція виведення інформаційного повідомлення
 const numberCountriesMore = () => {
@@ -27,24 +51,17 @@ const numberCountriesMore = () => {
     'Too many matches found. Please enter a more specific name.'
   );
 };
-const CountriesNot = () => {
-  Notiflix.Notify.failure('Oops, there is no country with that name');
-};
+// const CountriesNot = () => {
+//   Notiflix.Notify.failure('Oops, there is no country with that name');
+// };
 
-// Функція генерування розмітки для країн 2-10
-const listCountries = countries => {
-  console.log(countries.flags.svg);
-  console.log(countries.capital);
-  console.log(countries.population);
-  console.log(countries.languages);
-  console.log(countries.name.official);
-};
-
-// функція генерування одного елементу списку
+// функція генерування одного елементу списку країн
 const createListItem =
   item => `<li style="display: flex; align-items: center; margin: 5px;">
-<img src="${item.flags.svg}" alt="${item.flags.alt}" style="width: 30px; height: 20px; object-fit: cover; margin-right: 10px;">
-<span> ${item.name.official} </span>
+<img src="${item.flags.svg}" alt="${
+    item.flags.alt
+  }" style="width: 30px; object-fit: cover; margin-right: 10px;">
+<span id="list-li"> ${item.name.official ?? ''} </span>
 </li>`;
 // Функція розмноження рядка розмітки для всих елементів масиву країн
 const generateContent = array =>
@@ -52,7 +69,7 @@ const generateContent = array =>
 
 // функція генерування розмітки на 1 країну
 
-// функція створення масиву мов з обєкта
+// функція створення масиву мов з обєкта використані тернарні оператори перевірки наявності значення ?? 
 const createItem = item => {
   const arrayLang = Object.values(item.languages);
   return arrayLang.map(word => ' ' + word);
@@ -61,19 +78,19 @@ const createItemHTML = itemOb => {
   const item = itemOb[0];
   const arrayLangSpaces = createItem(item);
   return `
-  <p> <span class="name-country-data">Capital:</span> ${item.capital} </p>
-  <p> <span style="font-weight: bold;">Population: </span> ${item.population}</p>
-  <p> <span style="font-weight: bold;">Languages:</span> ${arrayLangSpaces}</p>`;
+  <p> <span class="name-country-data">Capital:</span> ${item.capital ?? ""} </p>
+  <p> <span style="font-weight: bold;">Population: </span> ${item.population ?? ""}</p>
+  <p> <span style="font-weight: bold;">Languages:</span> ${arrayLangSpaces ?? ""}</p>`;
 };
 //
-// функція додавання розмітки
+// функція додавання розмітки до списку країн
 const insertContent = array => {
   // const result = generateContent(array);
   const result = createItemHTML(array);
   listInfo.innerHTML = '';
   listInfo.insertAdjacentHTML('beforeend', result);
 };
-
+// функція додавання розмітки інформації про країну
 const insertContent1 = array => {
   const result = generateContent(array);
   clearList();
@@ -86,7 +103,7 @@ const handleInputChange = event => {
   console.log(inputEl.value);
   name1 = event.target.value.trim();
   if (name1 !== '') {
-    fetchCountries(name1);
+    fetchCountries(name1, selectionCountryFunction);
   } else {
     numberCountriesMore();
     clearList();
@@ -107,15 +124,19 @@ const selectionCountryFunction = array => {
     insertContent1(array);
     if (array.length === 1) {
       insertContent(array);
+      const listLi = document.getElementById('list-li');
+      listLi.classList.add('country-list-element');
     }
-  } else {
-    CountriesNot();
-    clearList();
   }
+  // Якщо не використовувати відловлювання 404 можна просто орієнтуватись на наявність даних
+    // else {
+  //   CountriesNot();
+  //   clearList();
+  // }
 };
 // функція затримки обробки введеного тексту
 const debouncedInputChange = debounce(handleInputChange, 300);
 // відслідковування ведденя назви країни
 inputEl.addEventListener('input', debouncedInputChange);
 
-// дописати класи на зміну розміру елементів при виборі 1єї країни, винести функцію в окремий док відповідності до тз 
+// дописати класи на зміну розміру елементів при виборі 1єї країни, винести функцію в окремий док відповідності до тз
